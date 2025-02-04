f53f1c1ab97d7d74437945329846a641e000072b
Vraj Bhingradiya <123621811+codewdme@users.noreply.github.com>
2025-02-04 23:51:48 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="w-full flex justify-center items-center">
       <p className="p-12 text-center text-gray-500 text-xs">
-        &copy;2023 EdgeRunners Corp. All rights reserved.
+        &copy;2025 Sneha Soni All rights reserved.
       </p>
     </div>
   );
